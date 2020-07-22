58462c77ccb773de79fcb561997150a9612dcf0f
Soroush Jahanzad <SoroushJahanzad@gmail.com>
2020-07-23 03:33:01 +0430

    Reverted the main body to production code

--- src/main/java/bankserver/Main.java
+++ src/main/java/bankserver/Main.java
@@ -2,17 +2,15 @@ package bankserver;
 
 public class Main {
     public static void main(String[] args) {
-//        String port, debug;
-//        port = "1234";
-//        debug = "1";
-//        try {
-//            Server server = new Server(Integer.parseInt(port), debug.equals("1"));
-//            server.start();
-//            System.out.println("Server started");
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-        Account account = new Account("hi", "hi", "hi", "hi");
-        account = new Account("hey", "hi", "hi", "hi");
+        String port, debug;
+        port = "1234";
+        debug = "1";
+        try {
+            Server server = new Server(Integer.parseInt(port), debug.equals("1"));
+            server.start();
+            System.out.println("Server started");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
